48e2e5fc389bd8f9f7789667660697ddc7bca2e7
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2023-11-16 13:07:43 +0200

    added basic structure for categories and installed sass

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
+import './categories.styles.scss';
+
 const App = () => {
+  // const [categories, setCategories] = useState([])
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        <img src='' />
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
+      {categories.map((category) => (
+        <div key={category.id} className='category-container'>
+          <img src='' />
+          <div className='background-image'></div>
           <div className='category-body-container'>
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-          <div className='category-body-container'>
-            <h2>Womens</h2>
-            <p>Shop Now</p>
-          </div>
-          <div className='category-body-container'>
-            <h2>Mens</h2>
+            <h2>{category.title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
