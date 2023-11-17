39195857668a4c1db59b76f1561d2c1c265974cf
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2023-11-17 16:21:22 +0200

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/Home';
 import Navigation from './routes/navigation/Navigation';
+import SignIn from './routes/sign-in/SignIn';
 
 const Shop = () => {
   return (
@@ -12,9 +13,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
