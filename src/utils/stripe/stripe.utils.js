811d0c9634e05e6e8f26149694bf246be5027b03
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2023-11-29 18:54:26 +0200

    fix stripe card element placeholders

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,3 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUPLISHABLE_KEY);
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUPLISHABLE_KEY, {
+  locale: 'en',
+});
